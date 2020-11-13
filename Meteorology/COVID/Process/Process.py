e29fc1ac47eb75973611e0271d80ae1fbf62b6c4
Arturon <anegreiross@outlook.com>
2020-11-13 12:56:50 -0500

    newCommit

---
+++ Meteorology/COVID/Process/Process.py
@@ -0,0 +1,15 @@
+import torch
+import numpy as np
+
+
+
+class Process:
+
+    def __init__(self):
+        self.url_name = ""
+
+    def Store(self, parameter_list):
+        """
+        docstring
+        """
+        pass
