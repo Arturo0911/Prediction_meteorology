154a5c031d9676e446c655e0d2735e26a89c3b9c
Arturon <anegreiross@outlook.com>
2020-11-13 18:47:48 -0500

    definded Prcess class

--- Meteorology/COVID/Process/Process.py
+++ Meteorology/COVID/Process/Process.py
@@ -6,20 +6,22 @@ import pandas as pd
 class Process:
 
     def __init__(self):
+
         self.url_name = "https://covid19.who.int/WHO-COVID-19-global-data.csv"
         self.data_frame = pd.read_csv(self.url_name)
 
     def Store(self):
-        """
-        docstring
-        """
-        pass
 
+        pass
+    
     def get_keys(self):
 
         keys_values = self.data_frame.columns
-
         return keys_values
 
-    def get_prediction_per_country(self):
+    def get_prediction_per_country(self, country):
+
+        pass
+
+    def get_scatter_points(self):
         pass
