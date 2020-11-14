588304b50156c5520ed14cb89025b76401f48b3c
Arturon <anegreiross@outlook.com>
2020-11-13 22:19:55 -0500

    ok

--- Meteorology/COVID/Process/Math_model.py
+++ Meteorology/COVID/Process/Math_model.py
@@ -2,5 +2,22 @@
 
 class Math_model:
 
-    def __init__(self):
+    def __init__(self, b0, b1, x, y):
+
+        # where the x and y are the mean of columns x y 
+        
+        self.Beta_zero = b0
+        self.Beta_one = b1
+        self.Y = y
+        self.X = x
+
+    def math_model_(self):
+
         pass
+
+        
+
+    """
+    In the math model Y= β0 + β1X   
+    To right predictions
+    """
