5100ca0eba8322b3aebd5103a1b6821b3bf8b280
Arturon <anegreiross@outlook.com>
2020-11-15 18:33:53 -0500

    ok

--- Android/app/src/main/java/com/example/meteorology/DisplayMessageActivity.java
+++ Android/app/src/main/java/com/example/meteorology/DisplayMessageActivity.java
@@ -2,7 +2,9 @@ package com.example.meteorology;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class DisplayMessageActivity extends AppCompatActivity {
 
@@ -10,5 +12,18 @@ public class DisplayMessageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_message);
+
+        // Get the intent that started this activity and stract the string
+
+        Intent intent = getIntent();
+        String message = intent.getStringExtra(MainActivity.EXTRA_MESSAGE);
+
+
+        // Capture the layaout's TextView and set the string as its text
+
+        TextView textView = findViewById(R.id.textView);
+        textView.setText(message);
+
+
     }
 }
